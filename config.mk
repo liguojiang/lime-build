b8435f9852b24f616269d30befa00450419bc3c7
liguojiang <guojiang@591wifi.com>
2017-02-04 10:58:35 +0800

    Update config.mk

--- config.mk
+++ config.mk
@@ -1,6 +1,6 @@
 # config.mk
 DEV ?=
-OWRT_GIT ?= https://github.com/libremesh/openwrt.git -b 16.07
+OWRT_GIT ?= https://github.com/liguojiang/openwrt.git -b 16.07
 OWRT_SCM = git clone $(OWRT_GIT)
 LIME_GIT_RW = git@github.com:libremesh/lime-packages.git
 LIME_GIT_RO = git://github.com/libremesh/lime-packages.git
